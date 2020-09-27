f1285c7a0242d573c405b515a60a5895ae8c2665
Gleb <epifg@yandex.ru>
2020-09-27 20:24:24 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i <2; i++){
+    const a = prompt('Один из последних просмотренных фильмов?', ''), 
+    b = prompt('На сколько оцените его?', '');
+
+    personalMovieDB.movies[a] = b;
+}
+
+console.log(personalMovieDB);
